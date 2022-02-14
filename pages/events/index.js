46cf19159a62ffcdc87a6dab7b8d72d6053d9c48
kamrul91111 <kamrul.islam91111@gmail.com>
2022-02-15 10:44:29 +1100

    done with all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import {getAllEvents} from "../../dummy-data";
+import classes from "./events.module.css";
+
+// components
+import PageHeading from "./../../components/PageHeading/PageHeading";
+import EventComponent from "./../../components/EventsComponent/EventComponent";
 
 const Events = () => {
-  return (
-    <div>This page displays events</div>
-  )
-}
+  const events = getAllEvents(); //grab all events from dummy data.
 
-export default Events
+  return (
+    <div>
+      <PageHeading text="events" color="brown" />
+      <div className={classes.eventsContainer}>
+        {events.map(i => (
+          <EventComponent item={i} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Events;
