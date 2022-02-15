e4239af57c8e256bcc06b54de37c0d16632df0bd
kamrul91111 <kamrul.islam91111@gmail.com>
2022-02-15 12:17:37 +1100

    done with filtering

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,9 +1,37 @@
 import React from 'react'
+import { useRouter } from 'next/router'
+import { getFilteredEvents } from './../../dummy-data'
+import PageHeading from './../../components/PageHeading/PageHeading'
+import EventComponent from './../../components/EventsComponent/EventComponent'
 
 const FilterEvents = () => {
-  return (
-    <div>Filter Events</div>
-  )
+  const router = useRouter()
+
+  const filterData = router.query.slug
+  const filteredYear = filterData[0]
+  const filteredMonth = filterData[1]
+
+  // grab filteredEvents from dummy data
+  const filteredEvents = getFilteredEvents({
+    year: Number(filteredYear),
+    month: Number(filteredMonth)
+  })
+
+  // if no data, or not numbers in params and success case
+  if (!filterData) {
+    return <p>No data found</p>
+  } else if (isNaN(filteredYear) || isNaN(filteredMonth)) {
+    return <p>Invalid URL</p>
+  } else {
+    return (
+      <div>
+        <PageHeading text='Events' color='gainsboro' />
+        {filteredEvents.map(f => (
+          <EventComponent item={f} />
+        ))}
+      </div>
+    )
+  }
 }
 
 export default FilterEvents
