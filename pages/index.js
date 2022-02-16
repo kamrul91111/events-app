ceeb7cb5b5ff06ff0f17a3c45a1e90e4e75b75a5
kamrul91111 <kamrul.islam91111@gmail.com>
2022-02-16 12:07:27 +1100

    static regen for products

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,32 @@
-import styles from "../styles/Home.module.css";
-import {getFeaturedEvents} from "./../dummy-data";
+import styles from '../styles/Home.module.css'
+import { getFeaturedEvents } from './../dummy-data'
+import Link from "next/link";
 
 // components
-import PageHeading from "../components/PageHeading/PageHeading";
-import EventComponent from "../components/EventsComponent/EventComponent";
-
+import PageHeading from '../components/PageHeading/PageHeading'
+import EventComponent from '../components/EventsComponent/EventComponent'
 
 const Home = () => {
   //this function returns all events with a featured flag
-  const featuredEvents = getFeaturedEvents();
+  const featuredEvents = getFeaturedEvents()
 
   return (
     <div className={styles.container}>
-      <PageHeading text="Exciting Events" color='limegreen' />
+      <PageHeading text='Exciting Events' color='limegreen' />
       <div className={styles.itemContainer}>
         {featuredEvents.map(f => (
           <EventComponent key={f.id} item={f} />
         ))}
       </div>
+      {/* banner */}
+      <div className={styles.banner}>
+        <h2>Grab Sweet Merch</h2>
+        <Link href='/products'>
+          <button className={styles.button}>Visit Merch Store</button>
+        </Link>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
---
+++ pages/products/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import classes from './products.module.css'
+import fs from 'fs/promises'
+import path from 'path'
+
+// components
+import PageHeading from './../../components/PageHeading/PageHeading'
+
+// destructure props and grab products
+const Products = ({ products }) => {
+  return (
+    <div>
+      <PageHeading text='Products' color='darkgreen' />
+      {products.map(p => (
+        <div key={p.id}>
+          <h4>{p.title}</h4>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+// for static regen by nextjs, server side code
+export const getStaticProps = async () => {
+  // file path current working directory's root
+  const filePath = path.join(process.cwd(), 'dummy-products.json')
+  // async function grab file
+  const jsonData = await fs.readFile(filePath)
+  // convert json to a js object
+  const data = JSON.parse(jsonData)
+
+  return {
+    props: {
+      products: data.products
+    }
+  }
+}
+
+export default Products
